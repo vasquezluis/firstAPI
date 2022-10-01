9e4fdd5153095c7aedeea79bbd3495883b45855b
LuiiVasquez <luivasque95@gmail.com>
2022-09-30 23:03:16 -0600

    Configuracion de controlador de productos

--- src/products/index.js
+++ src/products/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
+// obtener el controlador
+const { ProductsController } = require('./controller')
+
 // permite manejar las rutas del modulo
 const router = express.Router();
 
 module.exports.ProductsAPI = (app) => {
   router
-    .get('/', (req, res) => {}) // http://localhost:3000/api/products/
-    .get('/:id', (req, res) => {}) // http://localhost:3000/api/products/23
-    .post('/', (req, res) => {}) 
+    .get('/', ProductsController.getProducts) // http://localhost:3000/api/products/
+    .get('/:id', ProductsController.getProduct) // http://localhost:3000/api/products/23
+    .post('/', ProductsController.createProduct) 
 
   // hacer disponibles todas las rutas
   app.use('/api/products', router);
